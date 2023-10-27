f71530ddd662177975a3060af243cafeb3a29772
repour <poorsadeghi.r@gmail.com>
2023-10-28 01:38:59 +0330

    download cv

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cv.pdf';
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://flowcv.com/resume/u1aa92krsc" download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
